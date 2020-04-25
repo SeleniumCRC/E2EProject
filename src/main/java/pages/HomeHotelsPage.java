840ec0e8a09912c979417e9673f4bc2056fa4d5f
Chetan <cchopade79@gmail.com>
2020-04-25 21:35:53 +0530

    Third commit

--- src/main/java/pages/HomeHotelsPage.java
+++ src/main/java/pages/HomeHotelsPage.java
@@ -6,20 +6,21 @@ import org.openqa.selenium.support.PageFactory;
 
 import base.TestBase;
 
-public class HomeHotelsPage extends TestBase {
+public class HomeHotelsPage extends TestBase{
 
 	@FindBy(xpath = "//*[@class='menu-horizontal-wrapper-02']//li[1]/a")
 	private WebElement hotelsHeaderButton;
-
+	
+		//*[@id="s2id_autogen1"]//a
 	@FindBy(xpath = "//*[@id='s2id_autogen1']//a")
 	private WebElement hotelsDestination;
 
 	@FindBy(xpath = "//*[@id='airDatepickerRange-hotel']//*[@id='checkin']")
 	private WebElement hotelsCheckIn;
-
+	
 	@FindBy(xpath = "//*[@id='airDatepickerRange-hotel']//*[@id='checkout']")
 	private WebElement hotelsCheckOut;
-
+	
 	public HomeHotelsPage() {
 
 		PageFactory.initElements(driver, this);
@@ -40,5 +41,7 @@ public class HomeHotelsPage extends TestBase {
 	public WebElement getHotelsCheckOut() {
 		return hotelsCheckOut;
 	}
+	
 
 }
+	
--- src/test/java/tests/HomeHotelsTest.java
+++ src/test/java/tests/HomeHotelsTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -37,6 +38,18 @@ public class HomeHotelsTest extends TestBase {
 		System.out.println("On Home hotels page ");
 	}
 
+	@Test
+	public void fillDetailsTC2() {
+		homeHotel.getHotelsDestination().click();
+		homeHotel.getHotelsDestination().sendKeys("ind");
+		homeHotel.getHotelsDestination().sendKeys(Keys.DOWN);
+		homeHotel.getHotelsDestination().sendKeys(Keys.DOWN);
+		homeHotel.getHotelsDestination().sendKeys(Keys.ENTER);
+		
+		
+		System.out.println("destination");
+		
+	}
 	@AfterMethod
 	public void tearDown() {
 		driver.close();
